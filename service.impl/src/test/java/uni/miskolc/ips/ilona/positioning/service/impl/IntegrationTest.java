8fd5e455d8e9d04b949d1f7228f19ee8b4898483
Satan Adam <satanadam@outlook.com>
2018-07-12 09:21:17 +0200

    DTO converter classes added, measurementGateway using classes fixed

--- service.impl/src/test/java/uni/miskolc/ips/ilona/positioning/service/impl/IntegrationTest.java
+++ service.impl/src/test/java/uni/miskolc/ips/ilona/positioning/service/impl/IntegrationTest.java
@@ -4,7 +4,10 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import uni.miskolc.ips.ilona.measurement.controller.dto.MeasurementDTO;
 import uni.miskolc.ips.ilona.measurement.controller.dto.ZoneDTO;
+import uni.miskolc.ips.ilona.positioning.service.gateway.MeasurementGateway;
+import uni.miskolc.ips.ilona.positioning.service.gateway.MeasurementGatewaySIConfig;
 import uni.miskolc.ips.ilona.positioning.service.gateway.ZoneQueryService;
 import uni.miskolc.ips.ilona.positioning.service.gateway.ZoneQueryServiceSIConfig;
 
@@ -24,8 +27,6 @@ public class IntegrationTest {
 
         String zoneDTO = zoneQueryService.getZoneById("183f0204-5029-4b33-a128-404ba5c68fa8").getName();
         Assert.assertTrue(zoneDTO.equals("bedroom"));
-        zoneDTO = zoneQueryService.getZoneById("183f0204-5029-4b33-a128-404ba5c68fa").getName();
-        Assert.assertTrue(zoneDTO.equals("Unknown"));
         zoneDTO = zoneQueryService.getZoneById("743d2365-2eaa-412f-8324-6b6b1361ba5b").getName();
         Assert.assertTrue(zoneDTO.equals("kitchen"));
     }
@@ -41,6 +42,20 @@ public class IntegrationTest {
         /*for (Object zoneDTO : zoneDTOS) {
             System.out.println(zoneDTO);
         }*/
+
+    }
+
+    @Test
+    public void testListMeasurements() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(MeasurementGatewaySIConfig.class);
+
+        MeasurementGateway measurementGateway = context.getBean("MeasurementQueryGateway", MeasurementGateway.class);
+
+        Collection<MeasurementDTO> measurementDTOS = measurementGateway.listMeasurements();
+        //Assert.assertTrue(measurementDTOS.size() == 3);
+        for (Object measurementDTO : measurementDTOS) {
+            System.out.println(measurementDTO);
+        }
 
     }
 }
--- service.impl/src/test/java/uni/miskolc/ips/ilona/positioning/service/impl/classification/bayes/NaiveBayesTest.java
+++ service.impl/src/test/java/uni/miskolc/ips/ilona/positioning/service/impl/classification/bayes/NaiveBayesTest.java
@@ -9,6 +9,7 @@ import uni.miskolc.ips.ilona.measurement.model.position.Position;
 import uni.miskolc.ips.ilona.measurement.model.position.Zone;
 import uni.miskolc.ips.ilona.positioning.exceptions.InvalidMeasurementException;
 import uni.miskolc.ips.ilona.positioning.service.gateway.MeasurementGateway;
+import uni.miskolc.ips.ilona.positioning.service.impl.MeasurementDTOConverter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,11 +26,11 @@ public class NaiveBayesTest {
     private MeasurementDistanceCalculator distanceCalculator;
     private ArrayList<Measurement> measurementsList;
     private Measurement incomingMeasurement;
-    private Zone z1,z2,z3;
+    private Zone z1, z2, z3;
     private NaiveBayesPositioningService bayesPositioning;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         setUpZones();
         setUpMeasurements();
 
@@ -45,33 +46,33 @@ public class NaiveBayesTest {
     @Test
     public void emptyMeasurementsListTest() throws InvalidMeasurementException {
         measurementGateway = EasyMock.createMock(MeasurementGateway.class);
-        EasyMock.expect(measurementGateway.listMeasurements()).andReturn(new ArrayList<Measurement>());
+        EasyMock.expect(MeasurementDTOConverter.convertToMeasurements(measurementGateway.listMeasurements())).andReturn(new ArrayList<Measurement>());
         EasyMock.replay(measurementGateway);
 
-        bayesPositioning = new NaiveBayesPositioningService(measurementGateway,distanceCalculator,100);
+        bayesPositioning = new NaiveBayesPositioningService(measurementGateway, distanceCalculator, 100);
         Position actual = bayesPositioning.determinePosition(incomingMeasurement);
         Position expected = new Position(Zone.UNKNOWN_POSITION);
-        Assert.assertEquals(actual.getZone(),expected.getZone());
+        Assert.assertEquals(actual.getZone(), expected.getZone());
 
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void negativeMaxDistanceTest() {
         mockingMeasurementGateway();
-        bayesPositioning = new NaiveBayesPositioningService( measurementGateway, distanceCalculator,-100);
+        bayesPositioning = new NaiveBayesPositioningService(measurementGateway, distanceCalculator, -100);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void nullGatewayTest() {
-        bayesPositioning = new NaiveBayesPositioningService( null, distanceCalculator,100);
+        bayesPositioning = new NaiveBayesPositioningService(null, distanceCalculator, 100);
     }
 
 
     @Test
     public void workingAsExpected() throws InvalidMeasurementException {
         mockingMeasurementGateway();
-        bayesPositioning = new NaiveBayesPositioningService( measurementGateway, distanceCalculator,100);
-        Position result =bayesPositioning.determinePosition(incomingMeasurement);
+        bayesPositioning = new NaiveBayesPositioningService(measurementGateway, distanceCalculator, 100);
+        Position result = bayesPositioning.determinePosition(incomingMeasurement);
         assertNotNull(result);
 
     }
@@ -79,14 +80,14 @@ public class NaiveBayesTest {
     private void setUpMeasurements() {
         measurementsList = new ArrayList<Measurement>();
         MeasurementBuilder measbuilder = new MeasurementBuilder();
-		/*
-		 *
-		 */
+        /*
+         *
+         */
         BluetoothTags bluetooth = new BluetoothTags(
-                new HashSet<String>(Arrays.asList(new String[] { "001060AA36F8", "001060AA36F4", "001060AA36F2" })));
+                new HashSet<String>(Arrays.asList(new String[]{"001060AA36F8", "001060AA36F4", "001060AA36F2"})));
         Magnetometer magneto = new Magnetometer(12, 32, 23, 0.5);
         RFIDTags rfid = new RFIDTags(new HashSet<byte[]>());
-        rfid.addTag(new byte[] { (byte) 12 });
+        rfid.addTag(new byte[]{(byte) 12});
         WiFiRSSI wifi = new WiFiRSSI();
         wifi.setRSSI("egy", -0.4);
         wifi.setRSSI("Ketto", -1.2);
@@ -97,14 +98,14 @@ public class NaiveBayesTest {
         measbuilder.setWifiRSSI(wifi);
         measbuilder.setPosition(new Position(z2));
         measurementsList.add(measbuilder.build());
-		/*
-		 *
-		 */
+        /*
+         *
+         */
         BluetoothTags bluetooth2 = new BluetoothTags(
-                new HashSet<String>(Arrays.asList(new String[] { "001060AA36F8", "001060AA36F4", "001060AA36F2" })));
+                new HashSet<String>(Arrays.asList(new String[]{"001060AA36F8", "001060AA36F4", "001060AA36F2"})));
         Magnetometer magneto2 = new Magnetometer(12, 32, 23, 0.5);
         RFIDTags rfid2 = new RFIDTags(new HashSet<byte[]>());
-        rfid2.addTag(new byte[] { (byte) 12, (byte) 46 });
+        rfid2.addTag(new byte[]{(byte) 12, (byte) 46});
         WiFiRSSI wifi2 = new WiFiRSSI();
         wifi2.setRSSI("egy", -0.4);
         wifi2.setRSSI("Ketto", -1.2);
@@ -115,14 +116,14 @@ public class NaiveBayesTest {
         measbuilder.setWifiRSSI(wifi2);
         measbuilder.setPosition(new Position(z2));
         measurementsList.add(measbuilder.build());
-		/*
-		 *
-		 */
+        /*
+         *
+         */
         BluetoothTags bluetooth3 = new BluetoothTags(
-                new HashSet<String>(Arrays.asList(new String[] { "001060AA36F8", "001060AA36F4", "001060AA36F2" })));
+                new HashSet<String>(Arrays.asList(new String[]{"001060AA36F8", "001060AA36F4", "001060AA36F2"})));
         Magnetometer magneto3 = new Magnetometer(12, 32, 23, 0.5);
         RFIDTags rfid3 = new RFIDTags(new HashSet<byte[]>());
-        rfid3.addTag(new byte[] { (byte) 12, (byte) 46 });
+        rfid3.addTag(new byte[]{(byte) 12, (byte) 46});
         WiFiRSSI wifi3 = new WiFiRSSI();
         wifi3.setRSSI("Egy", -0.4);
         wifi3.setRSSI("Ketto", -1.2);
@@ -133,14 +134,14 @@ public class NaiveBayesTest {
         measbuilder.setWifiRSSI(wifi3);
         measbuilder.setPosition(new Position(z1));
         measurementsList.add(measbuilder.build());
-		/*
-		 *
-		 */
+        /*
+         *
+         */
         BluetoothTags bluetooth4 = new BluetoothTags(
-                new HashSet<String>(Arrays.asList(new String[] { "001060AA36F8", "001060AA36F4", "001060AA36F2" })));
+                new HashSet<String>(Arrays.asList(new String[]{"001060AA36F8", "001060AA36F4", "001060AA36F2"})));
         Magnetometer magneto4 = new Magnetometer(12, 32, 23, 0.5);
         RFIDTags rfid4 = new RFIDTags(new HashSet<byte[]>());
-        rfid4.addTag(new byte[] { (byte) 12, (byte) 46 });
+        rfid4.addTag(new byte[]{(byte) 12, (byte) 46});
         WiFiRSSI wifi4 = new WiFiRSSI();
         wifi4.setRSSI("egy", -0.4);
         wifi4.setRSSI("Ketto", -1.2);
@@ -151,9 +152,9 @@ public class NaiveBayesTest {
         measbuilder.setWifiRSSI(wifi4);
         measbuilder.setPosition(new Position(z2));
         measurementsList.add(measbuilder.build());
-		/*
-		 *
-		 */
+        /*
+         *
+         */
         wifi4.setRSSI("Negy", -12.2);
         wifi4.setRSSI("Ot", -6.2);
         measbuilder.setWifiRSSI(wifi4);
@@ -170,7 +171,7 @@ public class NaiveBayesTest {
 
     private void mockingMeasurementGateway() {
         measurementGateway = EasyMock.createMock(MeasurementGateway.class);
-        EasyMock.expect(measurementGateway.listMeasurements()).andReturn(measurementsList);
+        EasyMock.expect(MeasurementDTOConverter.convertToMeasurements(measurementGateway.listMeasurements())).andReturn(measurementsList);
         EasyMock.replay(measurementGateway);
 
     }
